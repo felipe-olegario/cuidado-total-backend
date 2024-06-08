0e6158370dfd78bb515700c7b2dae32d6b83eeee
igoresilva <igor082011@gmail.com>
2024-06-08 18:56:41 -0300

    created project setup

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:20-alpine as node
+
+WORKDIR /usr/app
+
+COPY package.json package-lock.json ./
+
+RUN npm install
+COPY . .
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const fastifyAdapter = new FastifyAdapter();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    fastifyAdapter,
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Cuidado Total')
+    .setDescription('...')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const HOST = process.env.HOST || '127.0.0.1';
+  const PORT = Number(process.env.PORT) || 3000;
+  app.enableCors({ origin: [/^(.*)/] });
+
+  await app.listen(PORT, HOST, () => {
+    console.log(`Listening on: ${HOST}:${PORT}`);
+  });
+}
+
+bootstrap();
