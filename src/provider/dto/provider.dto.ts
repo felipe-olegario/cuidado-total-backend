7248968fafdbaca30de4a3bed996a18f504383ad
felipe-olegario <felipe.olegario@bandtec.com.br>
2024-06-18 20:49:04 -0300

    feat: adjust swagger and tsconfig

--- src/provider/dto/provider.dto.ts
+++ src/provider/dto/provider.dto.ts
@@ -1,45 +1,56 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateProviderDto {
   @IsString()
+  @ApiProperty()
   @IsNotEmpty()
   name: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   phone: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   street: string;
 
   @IsString()
+  @ApiProperty()
   @IsNotEmpty()
   number: string;
 
   @IsString()
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 
   @IsString()
+  @ApiProperty()
   @IsNotEmpty()
   postalCode: string;
 
   @IsString()
+  @ApiProperty()
   @IsNotEmpty()
   document: string;
 }
 
 export class LoginDto {
   @IsEmail()
+  @ApiProperty()
   @IsNotEmpty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
